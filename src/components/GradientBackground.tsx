c116407a07187bb70fe65b7f4606f24422f5a43f
Hugo Caminada <caminada@redkiwi.nl>
2021-04-08 10:00:33 +0200

    Fixed background color.

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -10,7 +10,7 @@ const MainContainer = styled.View`
 
 const BottomHalf = styled.View`
   flex: 1;
-  background-color: rgba(187, 107, 217, 0.6);
+  background-color: rgba(132, 176, 235, 0.8);
 `
 const Content = styled.View`
   position: absolute;

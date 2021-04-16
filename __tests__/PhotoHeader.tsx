96e7318e0f96ae22aa0e392f9de9d639b9776cd0
Hugo Caminada <caminada@redkiwi.nl>
2021-04-16 13:17:30 +0200

    removed redundant import

--- __tests__/PhotoHeader.tsx
+++ __tests__/PhotoHeader.tsx
@@ -4,7 +4,6 @@
 
 import 'react-native'
 import React from 'react'
-import {Text} from 'react-native'
 import PhotoHeader from '../src/components/PhotoHeader'
 
 // Note: test renderer must be required after react-native.

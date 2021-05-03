f31e617d336313a773e45e445fe1ecd968cafe5c
Hugo Caminada <caminada@redkiwi.nl>
2021-05-03 15:16:44 +0200

    Resolved MR feedback.

--- src/app/hooks/useGeolocation.ts
+++ src/app/hooks/useGeolocation.ts
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
-import Geolocation from '@react-native-community/geolocation'
+import Geolocation, {GeolocationError} from '@react-native-community/geolocation'
 
 interface GeolocationData {
   latitude: number;
   longitude: number;
 }
 
-export const useGeolocation = (): [string, GeolocationData] => {
-  const [error, setError] = useState('')
+export const useGeolocation = (): [GeolocationError | null, GeolocationData] => {
+  const [error, setError] = useState<GeolocationError | null>(null)
   const [position, setPosition] = useState<GeolocationData>({
     latitude: 0,
     longitude: 0,
@@ -16,13 +16,16 @@ export const useGeolocation = (): [string, GeolocationData] => {
   useEffect(() => {
     const watchId = Geolocation.watchPosition(
       pos => {
-        setError('')
+        setError(null)
         setPosition({
           latitude: pos.coords.latitude,
           longitude: pos.coords.longitude,
         })
       },
-      e => setError(e.message)
+      e => setError(e),
+      {
+        enableHighAccuracy: true,
+      }
     )
     return () => Geolocation.clearWatch(watchId)
   }, [])

acb98038777870524210f5eb44383aae406b5f7c
Hugo Caminada <caminada@redkiwi.nl>
2021-05-07 10:09:07 +0200

    Added Icons

--- src/components/Cards/StatisticsCard.tsx
+++ src/components/Cards/StatisticsCard.tsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import Card from './Card'
 import {useAppSelector} from '../../app/hooks/redux'
 import {colors} from '../../constants'
 
 const MainContainer = styled.View`
-  justify-content: space-between;
+  justify-content: center;
   padding: 20px;
 `
 
 const MainText = styled.Text`
   color: ${colors.red};
-  margin-bottom: 10px;
+  font-size: 18px;
   font-weight: bold;
+  padding-left: 8px;
+`
+
+const RowContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  height: 30px;
+  margin-bottom: 10px;
+`
+
+const Circle = styled.View`
+  background-color: ${colors.red};
+  height: 30px;
+  width: 30px;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
 `
 
 const StatisticsCard = () => {
@@ -25,9 +44,24 @@ const StatisticsCard = () => {
   return (
     <Card title="Jouw Statestieken">
       <MainContainer>
-        <MainText>{travelledJourneys} Ritten</MainText>
-        <MainText>{seenAttractions} Bezienswaardigheden gespot</MainText>
-        <MainText>{savedCo2} gram Co2 bespaard</MainText>
+        <RowContainer>
+          <Circle>
+            <MaterialIcon name="tram" color={colors.white} size={25} />
+          </Circle>
+          <MainText>{travelledJourneys} Ritten</MainText>
+        </RowContainer>
+        <RowContainer>
+          <Circle>
+            <FontAwesomeIcon name="university" color={colors.white} size={20} />
+          </Circle>
+          <MainText>{seenAttractions} Bezienswaardigheden gespot</MainText>
+        </RowContainer>
+        <RowContainer>
+          <Circle>
+            <FontAwesomeIcon name="tree" color={colors.white} size={20} />
+          </Circle>
+          <MainText>{savedCo2} gram Co2 bespaard</MainText>
+        </RowContainer>
       </MainContainer>
     </Card>
   )

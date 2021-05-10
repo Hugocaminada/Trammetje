e65488ee593ae1f0ff7f2528ae96314b4739b5e2
Hugo Caminada <caminada@redkiwi.nl>
2021-05-10 13:10:50 +0200

    Deleted unused import

--- src/components/Cards/StatisticsCard.tsx
+++ src/components/Cards/StatisticsCard.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import styled from 'styled-components/native'
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
